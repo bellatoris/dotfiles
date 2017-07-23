ec0b2b9c825a1bdb7f822a26e52575f39c68d142
bellatoris <bellatoris@snu.ac.kr>
2017-07-23 21:17:00 +0900

    [dotfiles] add tmux configuration

--- setup.scala
+++ setup.scala
@@ -1,31 +1,32 @@
-import java.io._
 import java.nio.file._
 import scala.collection.immutable.HashMap
+import sys.process._
 
 object Main {
-  /*
+
+  // helper class for colorise console
   implicit class ConsoleColorise(val str: String) extends AnyVal {
     import Console._
 
-    def black     = s"$BLACK$str"
-    def red       = s"$RED$str"
-    def green     = s"$GREEN$str"
-    def yellow    = s"$YELLOW$str"
-    def blue      = s"$BLUE$str"
-    def magenta   = s"$MAGENTA$str"
-    def cyan      = s"$CYAN$str"
-    def white     = s"$WHITE$str"
+    def black     = s"$BLACK$str$RESET"
+    def red       = s"$RED$str$RESET"
+    def green     = s"$GREEN$str$RESET"
+    def yellow    = s"$YELLOW$str$RESET"
+    def blue      = s"$BLUE$str$RESET"
+    def magenta   = s"$MAGENTA$str$RESET"
+    def cyan      = s"$CYAN$str$RESET"
+    def white     = s"$WHITE$str$RESET"
 
-    def blackBg   = s"$BLACK_B$str"
-    def redBg     = s"$RED_B$str"
-    def greenBg   = s"$GREEN_B$str"
-    def yellowBg  = s"$YELLOW_B$str"
-    def blueBg    = s"$BLUE_B$str"
-    def magentaBg = s"$MAGENTA_B$str"
-    def cyanBg    = s"$CYAN_B$str"
-    def whiteBg   = s"$WHITE_B$str"
+    def blackBg   = s"$BLACK_B$str$RESET"
+    def redBg     = s"$RED_B$str$RESET"
+    def greenBg   = s"$GREEN_B$str$RESET"
+    def yellowBg  = s"$YELLOW_B$str$RESET"
+    def blueBg    = s"$BLUE_B$str$RESET"
+    def magentaBg = s"$MAGENTA_B$str$RESET"
+    def cyanBg    = s"$CYAN_B$str$RESET"
+    def whiteBg   = s"$WHITE_B$str$RESET"
   }
-  */
+
 
   def main(args: Array[String]): Unit = {
     print("""
@@ -37,13 +38,13 @@ object Main {
       |╚═════╝  ╚═════╝   ╚═╝   ╚═╝     ╚═╝╚══════╝╚══════╝╚══════╝
       |
       |https://github.com/bellatoris/dotfiles
-      |""".stripMargin)
+      |""".red.stripMargin)
 
     install()
   }
 
   def install(): Unit = {
-    val filesHere = (new File(".")).listFiles 
+    val filesHere = new java.io.File(".").listFiles
     val tasks = HashMap[String, String](
       // shell
       // "/.bashrc" -> "bashrc",
@@ -58,33 +59,41 @@ object Main {
       "/.tmux.conf" -> "tmux/tmux.conf"
     )
 
-    val postActions = (
+    val postActions = List[String](
       // run vim-plug installation
       "vim +PlugInstall +qall"
     )
 
     doTask(tasks)
+    doAction(postActions)
   }
 
   def doTask(tasks: HashMap[String, String]): Unit = {
     for ((target, source) <- tasks) {
       // normalize file path
-      val currentPath = new File(".").getCanonicalPath()
+      val currentPath = new java.io.File(".").getCanonicalPath()
       val sourcePath = Paths.get(currentPath, source)
       val targetPath = Paths.get(System.getProperty("user.home"), target)
-      
+
       if (Files.exists(targetPath)) {
         if (Files.isSymbolicLink(targetPath)) {
           Files.delete(targetPath)
           Files.createSymbolicLink(targetPath, sourcePath)
-          println(target + " symlink created from " + sourcePath.toString())
+          println(target.blue + " symlink created from ".green + sourcePath.toString().green)
         } else {
-          println(target + " already exists but not a symbolic link")
+          println(target.blue + " already exists but not a symbolic link".magenta)
         }
       } else {
         Files.createSymbolicLink(targetPath, sourcePath)
-        println(target + " symlink created from " + sourcePath.toString())
+        println(target.blue + " symlink created from ".green + sourcePath.toString().green)
       }
+    }
+  }
+
+  def doAction(actions: List[String]): Unit = {
+    for (action <- actions) {
+      println("Execution: ".cyan + action.stripPrefix("").split("\n")(0))
+      action.!
     }
   }
 
---
+++ vim/plugins.vim
@@ -0,0 +1,8 @@
+let s:darwin = has('mac')
+
+" Plug buffers appear in a new tab
+let g:plug_window = '-tabnew'
+
+call plug#begin('~/.vim/plugged')
+
+call plug#end()
