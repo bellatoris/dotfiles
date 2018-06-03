bbcae1bead0732e66e7578d535dd4bd5e3a6a37c
bellatoris <bellatoris@snu.ac.kr>
2018-06-04 01:48:54 +0900

    [dotfiles] fzf-git moidfied

--- fzf-git.sh
+++ fzf-git.sh
@@ -13,7 +13,7 @@ gf() {
 	git -c color.status=always status --short |
 	fzf-down -m --ansi --nth 2..,.. \
 		--preview '(git diff --color=always -- {-1} | sed 1,4d; cat {-1}) | head -500' |
-	cut -c4 | sed 's/.* -> //'
+	cut -c4- | sed 's/.* -> //'
 }
 
 gb() {
